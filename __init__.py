695d1e0d24b2e44522c4d990a84138c11b0b9388
maddymadhan5330 <160740801+maddymadhan5330@users.noreply.github.com>
2024-02-28 22:49:31 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import json
+def getConfig(key):
+    # add your configuration path
+    con_file=r"c:\\Users\muthu\playground\blog\config.json"
+    file=open(con_file,'r')
+    config=json.loads(file.read())
+    file.close()
+    if key in config:
+        return config[key]
+    else:
+        raise Exception("ey {} is not found".format(key))
+    print("hello")
